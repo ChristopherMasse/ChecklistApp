457a92c9a3523becc177178edf02da79f6253a81
Chris Masse (P) <christophermassegithub@gmail.com>
2019-06-24 18:05:08 -0400

    added tests

---
+++ app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/edit_task/EditTaskFragTest.java
@@ -0,0 +1,8 @@
+package com.christophermasse.checklist.presentation.feature.edit_task;
+
+import static org.junit.Assert.*;
+
+@Run
+public class EditTaskFragTest {
+
+}
---
+++ app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/splash/SplashActivityTest.java
@@ -0,0 +1,7 @@
+package com.christophermasse.checklist.presentation.feature.splash;
+
+import static org.junit.Assert.*;
+
+public class SplashActivityTest {
+
+}
