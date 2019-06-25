2890ff67a88eb930d90696c7352c26d5d8d279c1
Chris Masse (P) <christophermassegithub@gmail.com>
2019-06-25 17:57:57 -0400

    added more test

--- app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/edit_task/EditTaskFragTest.java
+++ app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/edit_task/EditTaskFragTest.java
@@ -1,8 +1,29 @@
 package com.christophermasse.checklist.presentation.feature.edit_task;
 
-import static org.junit.Assert.*;
+import com.christophermasse.checklist.presentation.activity.BaseActivity;
+import com.christophermasse.checklist.presentation.feature.add_task.AddTaskActivity;
+import com.christophermasse.checklist.presentation.feature.task_list.TaskListActivity;
 
-@Run
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
 public class EditTaskFragTest {
+
+    @Rule
+    public ActivityScenarioRule<TaskListActivity> mActivityTestRule = new ActivityScenarioRule<>(TaskListActivity.class);
+
+
+
+    @Test
+    public void testFrag(){
+
+        Assert.assertEquals(2, 2);
+    }
 
 }
--- app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/splash/SplashActivityTest.java
+++ app/src/androidTest/java/com/christophermasse/checklist/presentation/feature/splash/SplashActivityTest.java
@@ -1,7 +1,15 @@
 package com.christophermasse.checklist.presentation.feature.splash;
 
+import org.junit.runner.RunWith;
+
+import androidx.test.runner.AndroidJUnit4;
+
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class SplashActivityTest {
+
+
+
 
 }
